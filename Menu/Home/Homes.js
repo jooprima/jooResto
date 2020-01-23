a7a62f8bb533312a266ecb88d4c1ddd46e16c036
jooprima <jooprima@gmail.com>
2020-01-24 01:30:10 +0700

    page home part 3

--- Menu/Home/Homes.js
+++ Menu/Home/Homes.js
@@ -1,8 +1,17 @@
 import React, {Component} from 'react';
-import {View, Text, Content, Button} from 'native-base';
+import {
+  Text,
+  Content,
+  Button,
+  Card,
+  CardItem,
+  Left,
+  Icon,
+  Right,
+} from 'native-base';
 import Footers from '../Footers.js';
 import ImageSlider from 'react-native-image-slider';
-import {StatusBar} from 'react-native';
+import {StatusBar, Image, View} from 'react-native';
 import Axios from 'axios';
 
 export default class Homes extends Component {
@@ -16,6 +25,7 @@ export default class Homes extends Component {
         'https://images.pexels.com/photos/326268/pexels-photo-326268.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       ],
       dataKategori: [],
+      dataRestaurant: [],
     };
   }
 
@@ -31,8 +41,22 @@ export default class Homes extends Component {
     });
   };
 
+  getDataRestaurant = () => {
+    Axios.get(
+      'https://developers.zomato.com/api/v2.1/search?start=6&count=10&sort=rating',
+      {
+        headers: {'user-key': '5c972128958a1988ac8af0f259c58e85'},
+      },
+    ).then(res => {
+      this.setState({
+        dataRestaurant: res.data.restaurants,
+      });
+    });
+  };
+
   componentDidMount() {
     this.getDataKategori();
+    this.getDataRestaurant();
   }
 
   render() {
@@ -41,18 +65,62 @@ export default class Homes extends Component {
         <StatusBar backgroundColor="#7457e8" />
 
         {/* ImageSlider */}
-        <View style={{height: 150}}>
-          <ImageSlider images={this.state.image} autoPlayWithInterval={3000} />
-        </View>
+        <Content>
+          <View style={{height: 150}}>
+            <ImageSlider
+              images={this.state.image}
+              autoPlayWithInterval={3000}
+            />
+          </View>
 
-        {/* Kategori */}
-        <Text style={{marginTop: 20, marginLeft: 10}}>Pilihan Kategori</Text>
-        <Content style={{marginTop: 20}} horizontal>
-          {this.state.dataKategori.map((data, key) => {
+          {/* Kategori */}
+          <Text style={{marginTop: 20, marginLeft: 10}}>Pilihan Kategori</Text>
+          <Content style={{marginTop: 20}} horizontal>
+            {this.state.dataKategori.map((data, key) => {
+              return (
+                <Button rounded key={key} style={{margin: 5}}>
+                  <Text>{data.categories.name}</Text>
+                </Button>
+              );
+            })}
+          </Content>
+
+          {/* Restoran terbaik */}
+          <Text style={{marginTop: 20, marginLeft: 20}}>
+            Restaurant Terbaik
+          </Text>
+
+          {this.state.dataRestaurant.map((data, key) => {
+            var image = '';
+            if (data.restaurant.thumb === '') {
+              image =
+                'https://images.pexels.com/photos/2788792/pexels-photo-2788792.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500';
+            } else {
+              image = data.restaurant.thumb;
+            }
             return (
-              <Button rounded key={key} style={{margin: 5}}>
-                <Text>{data.categories.name}</Text>
-              </Button>
+              <Card key={key}>
+                <CardItem>
+                  <Text>{data.restaurant.name}</Text>
+                </CardItem>
+                <CardItem cardBody>
+                  <Image
+                    style={{height: 200, width: null, flex: 1}}
+                    source={{
+                      uri: image,
+                    }}
+                  />
+                </CardItem>
+                <CardItem>
+                  <Left>
+                    <Icon name="star" style={{color:'#f2ed0d'}} />
+                    <Text>{data.restaurant.user_rating.aggregate_rating}</Text>
+                  </Left>
+                  <Right>
+                    <Text>{data.restaurant.user_rating.rating_text}</Text>
+                  </Right>
+                </CardItem>
+              </Card>
             );
           })}
         </Content>
