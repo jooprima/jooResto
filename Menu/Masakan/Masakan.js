749d207385ad13504764b7d5b5886d62111136cd
jooprima <jooprima@gmail.com>
2020-01-24 14:45:58 +0700

    page jenis masakan part 2

--- Menu/Masakan/Masakan.js
+++ Menu/Masakan/Masakan.js
@@ -11,9 +11,46 @@ import {
   Icon,
 } from 'native-base';
 import Footers from '../Footers';
+import Axios from 'axios';
 
 export default class Masakan extends Component {
   static navigationOptions = {headerShown: false};
+
+  constructor() {
+    super();
+    this.state = {
+      jenisMasakan: [],
+      dataRestaurant: [],
+    };
+  }
+
+  getJenisMasakan = () => {
+    Axios.get('https://developers.zomato.com/api/v2.1/cuisines?city_id=74', {
+      headers: {'user-key': '5c972128958a1988ac8af0f259c58e85'},
+    }).then(res => {
+      this.setState({
+        jenisMasakan: res.data.cuisines,
+      });
+    });
+  };
+
+  getDataRestaurant = () => {
+    Axios.get(
+      'https://developers.zomato.com/api/v2.1/search?start=30&count=10',
+      {
+        headers: {'user-key': '5c972128958a1988ac8af0f259c58e85'},
+      },
+    ).then(res => {
+      this.setState({
+        dataRestaurant: res.data.restaurants,
+      });
+    });
+  };
+
+  componentDidMount() {
+    this.getJenisMasakan();
+    this.getDataRestaurant();
+  }
 
   render() {
     return (
@@ -21,39 +58,57 @@ export default class Masakan extends Component {
         <Content>
           <Text style={{marginTop: 20, marginLeft: 10}}>Jenis Masakan</Text>
           <Content horizontal style={{marginTop: 20, marginLeft: 10}}>
-            <Button>
-              <Text>Masakan Jawa</Text>
-            </Button>
+            {this.state.jenisMasakan.map((data, key) => {
+              return (
+                <View key={key}>
+                  <Button style={{margin: 10}}>
+                    <Text>{data.cuisine.cuisine_name}</Text>
+                  </Button>
+                </View>
+              );
+            })}
           </Content>
           <Text style={{marginTop: 20, marginLeft: 10}}>Restaurant</Text>
           <Content horizontal style={{marginTop: 20, marginLeft: 10}}>
-            <Card>
-              <CardItem>
-                <Left>
-                  <Text>Nama Restaurant</Text>
-                </Left>
-                <Right>
-                  <Icon name="home" />
-                </Right>
-              </CardItem>
-              <CardItem cardBody>
-                <Image
-                  style={{height: 200, width: null, flex: 1}}
-                  source={{
-                    uri:
-                      'https://images.pexels.com/photos/2788792/pexels-photo-2788792.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-                  }}
-                />
-              </CardItem>
-              <CardItem>
-                <Left>
-                  <Text>Jenis Masakan</Text>
-                </Left>
-                <Right>
-                  <Text>Alamat</Text>
-                </Right>
-              </CardItem>
-            </Card>
+            {this.state.dataRestaurant.map((data, key) => {
+              var image = '';
+              if (data.restaurant.thumb === '') {
+                image =
+                  'https://images.pexels.com/photos/2788792/pexels-photo-2788792.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500';
+              } else {
+                image = data.restaurant.thumb;
+              }
+              return (
+                <Card key={key} style={{width: 300}}>
+                  <CardItem>
+                    <Left>
+                      <Text>{data.restaurant.name}</Text>
+                    </Left>
+                    <Right>
+                      <Icon name="home" />
+                    </Right>
+                  </CardItem>
+                  <CardItem cardBody>
+                    <Image
+                      style={{height: 200, width: null, flex: 1}}
+                      source={{
+                        uri: image,
+                      }}
+                    />
+                  </CardItem>
+                  <CardItem>
+                    <Left>
+                      <Text>Jenis Masakan</Text>
+                    </Left>
+                  </CardItem>
+                  <CardItem>
+                    <Left>
+                      <Text> {data.restaurant.cuisines} </Text>
+                    </Left>
+                  </CardItem>
+                </Card>
+              );
+            })}
           </Content>
         </Content>
         <Footers />
